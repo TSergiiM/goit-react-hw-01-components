224b7f1f104adf3c31c2e27531101ffaa39217e2
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-04 20:02:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
+import { Profile } from 'components/Profile/Profile';
+import user from 'user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from 'data.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics />
     </div>
   );
 };
