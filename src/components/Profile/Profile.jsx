1f16f86af58a3abee68b84a14426ec3858bbd6fa
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-05 01:50:47 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,38 @@
 import PropTypes from 'prop-types';
+import {
+  ProfileUser,
+  Description,
+  AvatarUser,
+  UserName,
+  UserStats,
+  UserStatsItem,
+  Quantity,
+} from './Profile.styled';
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className={avatar} />
-        <p className={username}>{username}</p>
-        <p className={tag}>@{tag}</p>
-        <p className={location}>{location}</p>
-      </div>
+    <ProfileUser>
+      <Description>
+        <AvatarUser src={avatar} alt={username} />
+        <UserName>{username}</UserName>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </Description>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <UserStats>
+        <UserStatsItem>
+          <span>Followers</span>
+          <Quantity>{stats.followers}</Quantity>
+        </UserStatsItem>
+        <UserStatsItem>
+          <span>Views</span>
+          <Quantity>{stats.views}</Quantity>
+        </UserStatsItem>
+        <UserStatsItem>
+          <span>Likes</span>
+          <Quantity>{stats.likes}</Quantity>
+        </UserStatsItem>
+      </UserStats>
+    </ProfileUser>
   );
 };
 Profile.propTypes = {
