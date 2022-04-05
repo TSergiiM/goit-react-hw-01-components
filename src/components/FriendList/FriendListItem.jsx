78c3f1c65bb0a6679a5ff8c996a016ea5f807f7f
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-05 12:29:40 +0300

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
+import {
+  FriendItem,
+  Status,
+  FriendAvatar,
+  FriendName,
+} from 'components/FriendList/Friends.styled';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className="item">
-      <span className="status">{isOnline}</span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
-    </li>
+    <FriendItem>
+      <Status>{isOnline}</Status>
+      <FriendAvatar src={avatar} alt="User avatar" width="48" />
+      <FriendName>{name}</FriendName>
+    </FriendItem>
   );
 };
 FriendListItem.propTypes = {
