0d71935e8a5008e9313e8e50c25ff0a4f7bd166d
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-05 12:15:01 +0300

    Create TransactionHistory.styled.js

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+export const Table = styled.table`
+  min-width: 350px;
+  background-color: gold;
+  text-align: center;
+`;
+export const TableHead = styled.thead`
+  background-color: blue;
+  color: white;
+`;
+export const TableRow = styled.tr`
+  height: 30px;
+`;
+export const TableData = styled.td`
+  border: 1px solid black;
+  background: white;
+`;
